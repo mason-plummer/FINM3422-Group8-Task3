5887c94f52b719d02665ac63c12b355667bb5aab
Mason Plummer <masonplummer857@gmail.com>
2026-05-06 20:05:45 +1000

    fix the mispelling in the data set

--- src/yieldcurve.py
+++ src/yieldcurve.py
@@ -1,1 +1,8 @@
 import pandas as pd
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # .../src
+DATA_PATH = os.path.join(BASE_DIR, '..', 'data', 'rba_yields.csv')
+
+df = pd.read_csv(DATA_PATH)
+
