1d0b1c17df49c7224cbdcf79b7b52e30c115ba04
Mason Plummer <masonplummer857@gmail.com>
2026-05-06 20:07:18 +1000

    catch up branch

--- src/yieldcurve.py
+++ src/yieldcurve.py
@@ -1,1 +1,6 @@
 import pandas as pd
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # .../src
+DATA_PATH = os.path.join(BASE_DIR, '..', 'data', 'rba_yields.csv')
+
+df = pd.read_csv(DATA_PATH)
