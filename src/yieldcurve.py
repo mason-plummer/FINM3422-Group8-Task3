e732ca1f1d01105c37ea8bea272b7dfd9e6fb9d6
Mason Plummer <masonplummer857@gmail.com>
2026-05-08 13:31:06 +1000

    initialisation function in yieldcurve.py

--- src/yieldcurve.py
+++ src/yieldcurve.py
@@ -1,17 +1,28 @@
-import pandas as pd
-import os
-import math
+import numpy as np 
 import matplotlib.pyplot as plt
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))  
-DATA_PATH = os.path.join(BASE_DIR, '..', 'data', 'rba_yields.csv')
+class YieldCurve:
+    """
+    YieldCurve represents a term structure of zero rates and provides discount factors for valuations. 
+    
+    """
+    def __init__(self, maturities, zero_rates, compounding = "coninuous")
+        """
+        Maturies are the time to maturity for each zero rate, and zero_rates are the corresponding zero rates - for reference these will be entered in as lists. The compounding method can be "continuous" or "annual" and will be entered using strings.
+        """
 
-df = pd.read_csv(DATA_PATH)
-row = df[df["Date"] == "31-Mar-2026"].iloc[0]
+        self.maturities = np.array(maturities, dtype=float)
+        self.zero_rates = np.array(zero_rates, dtype=float)
+        # want these to be numpy arrays for easier calculations and to ensure they are floats for the discount factor calculations.
+        self.compounding = compounding
+        # keep it as a string for the discount factor calculations, but could also be an enum or something else if we wanted to be more robust.
 
-data = [
-    (2.0,  row["Australian Government 2 Year Bond "]        / 100),
-    (3.0,  row["Australian Government 3 Year Bond"]         / 100),
-    (5.0,  row["Australian Government Bond 5 Year Bond"]    / 100),
-    (10.0, row["Australian Government 10 Year Bond"]        / 100),
-]
+        if len(self.maturities) != len(self.zero_rates):
+            raise ValueError("Maturities and zero rates must have the same length.")
+        
+        order = np.argsort(self.maturities)
+        # numpy function to sor the maturities and zero rates in ascending order of maturities, which is important for interpolation and discount factor calculations.
+        self.maturities = self.maturities[order]
+        self.zero_rates = self.zero_rates[order]
+
+        
