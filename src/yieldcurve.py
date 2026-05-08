b34d1bf1ccbcfb1f79c066d7d296f5e6b3b45552
Mason Plummer <masonplummer857@gmail.com>
2026-05-08 13:03:34 +1000

    reset

--- src/yieldcurve.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import os
-
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))  # .../src
-DATA_PATH = os.path.join(BASE_DIR, '..', 'data', 'rba_yields.csv')
-
-df = pd.read_csv(DATA_PATH)
-
