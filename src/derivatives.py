5e55d769729bd156dd313019a6e29327894d8586
Mason Plummer <masonplummer857@gmail.com>
2026-05-15 13:27:12 +1000

    define black scholes pricing for call option

--- src/derivatives.py
+++ src/derivatives.py
@@ -24,7 +24,32 @@ class Derivative:
      self.T = T
      self.sigma = sigma
      self.yield_curve = yield_curve
+     r = self.yield_curve.get_zero_rate(self.T) 
+     
+     # d1 and d2 
+     d1 = (
+        np.log(self.S0 / self.K) + (r + 0.5 * self.sigma ** 2) * self.T
+     ) / (self.sigma * np.sqrt(self.T))
+     d2 = d1 - self.sigma * np.sqrt(self.T)
+     
      # Also possible to define d1, d2 here as well.
 
 def price(self):
    return NotImplementedError("Pricing should be done within subclasses, not within the base class.")
+
+class EuropeanCall(Derivative):
+   """
+   European Call option priced using black-scholes
+   """
+
+# Don't need an initialisation function as it will call the one above. 
+
+   def price(self):
+      """
+      Return the black-scholes price for a european call option.
+      """
+      call_price = (
+      self.S0 * norm.cdf(self.d1) - self.K * np.exp(-r * self.T) * norm.cdf(self.d2)
+   )
+      return call_price
+   
